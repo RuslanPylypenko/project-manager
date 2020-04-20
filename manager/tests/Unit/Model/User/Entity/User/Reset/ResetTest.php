f07eb06b814ac4593397a2494a4ba9a213f2efe1
ruslan <ruslan.php.dev@gmail.com>
2020-04-20 15:03:26 +0300

    user builder

--- manager/tests/Unit/Model/User/Entity/User/Reset/ResetTest.php
+++ manager/tests/Unit/Model/User/Entity/User/Reset/ResetTest.php
@@ -3,18 +3,15 @@
 
 namespace App\Tests\Unit\Model\User\Entity\User\Reset;
 
-
-use App\Model\User\Entity\User\Email;
-use App\Model\User\Entity\User\Id;
 use App\Model\User\Entity\User\ResetToken;
-use App\Model\User\Entity\User\User;
+use App\Tests\Builder\User\UserBuilder;
 use PHPUnit\Framework\TestCase;
 
 class ResetTest extends TestCase
 {
     public function testSuccess()
     {
-        $user = $this->buildSignedUpByEmailUser();
+        $user = (new UserBuilder())->viaEmail()->build();
 
         $now = new \DateTimeImmutable();
         $token = new ResetToken('token', $now->modify('+1 day'));
@@ -32,7 +29,7 @@ class ResetTest extends TestCase
 
     public function testExpireToken(): void
     {
-        $user = $this->buildSignedUpByEmailUser();
+        $user = (new UserBuilder())->viaEmail()->build();
 
         $now = new \DateTimeImmutable();
         $token = new ResetToken('token', $now);
@@ -45,7 +42,7 @@ class ResetTest extends TestCase
 
     public function testNotRequested(): void
     {
-        $user = $this->buildSignedUpByEmailUser();
+        $user = (new UserBuilder())->viaEmail()->build();
 
         $now = new \DateTimeImmutable();
 
@@ -53,26 +50,5 @@ class ResetTest extends TestCase
         $user->passwordReset($now, 'hash');
     }
 
-
-    private function buildSignedUpByEmailUser(): User
-    {
-        $user = $this->buildUser();
-
-        $user->signUpByEmail(
-            new Email('test@app.test'),
-            'hash',
-            $token = 'token'
-        );
-
-        return $user;
-    }
-
-    private function buildUser(): User
-    {
-        return new User(
-            Id::next(),
-            new \DateTimeImmutable()
-        );
-    }
 
 }
