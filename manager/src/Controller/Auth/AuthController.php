7a81511fed59c9fd0a4be2e8cfb6d11ab37e500a
ruslan <ruslan.php.dev@gmail.com>
2020-04-23 19:46:50 +0300

    logout link

--- manager/src/Controller/Auth/AuthController.php
+++ manager/src/Controller/Auth/AuthController.php
@@ -24,4 +24,12 @@ class AuthController extends AbstractController
     }
 
 
+    /**
+     * @Route("/logout", name="app_logout", methods={"GET"})
+     */
+    public function logout(): Response
+    {
+        // controller can be blank: it will never be executed!
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    }
 }
