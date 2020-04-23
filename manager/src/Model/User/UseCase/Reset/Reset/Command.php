979a627fdd263b4bdf393d9be384d6078cb2eb4f
ruslan <ruslan.php.dev@gmail.com>
2020-04-23 16:26:19 +0300

    reset password action

--- manager/src/Model/User/UseCase/Reset/Reset/Command.php
+++ manager/src/Model/User/UseCase/Reset/Reset/Command.php
@@ -2,15 +2,19 @@
 
 namespace App\Model\User\UseCase\Reset\Reset;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class Command {
 
     /**
      * @var string
+     * @Assert\NotBlank()
      */
     public $passwordHash;
 
     /**
      * @var string
+     * @Assert\Length(min=6)
      */
     public $resetToken;
 
