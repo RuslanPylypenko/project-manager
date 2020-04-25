cef2d867b398396da3b5effc8fd637a5d0802e9a
ruslan <ruslan.php.dev@gmail.com>
2020-04-25 20:05:37 +0300

    facebook login page

--- manager/src/Security/UserProvider.php
+++ manager/src/Security/UserProvider.php
@@ -29,7 +29,7 @@ class UserProvider implements UserProviderInterface
     public function loadUserByUsername($username): UserInterface
     {
         $user = $this->loadUser($username);
-        return self::identityByUser($user);
+        return self::identityByUser($user, $username);
     }
 
     /**
@@ -41,7 +41,8 @@ class UserProvider implements UserProviderInterface
             throw new UnsupportedUserException('Invalid user class ' . \get_class($identity));
         }
 
-        return $identity;
+        $user = $this->loadUser($identity->getUsername());
+        return self::identityByUser($user, $identity->getUsername());
     }
 
     /**
@@ -54,18 +55,24 @@ class UserProvider implements UserProviderInterface
 
     private function loadUser($username): AuthView
     {
-        if (!$user = $this->users->findForAuth($username)) {
-            throw new UsernameNotFoundException('');
+        $chunks = explode(":", $username);
+
+        if(\count($chunks) === 2 && $user = $this->users->findForAuthByNetwork($chunks[0], $chunks[1])){
+            return $user;
         }
-        return $user;
+
+        if ($user = $this->users->findForAuthByEmail($username)) {
+            return $user;
+        }
+        throw new UsernameNotFoundException('');
     }
 
-    private static function identityByUser(AuthView $user): UserIdentity
+    private static function identityByUser(AuthView $user, string $username): UserIdentity
     {
         return new UserIdentity(
             $user->id,
-            $user->email,
-            $user->password_hash,
+            $username,
+            $user->password_hash ?: '',
             $user->role,
             $user->status
         );
