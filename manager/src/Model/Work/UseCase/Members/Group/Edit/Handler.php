d36d77c765f4b8de1d1de43afcd7aa0142c7a82d
ruslan <ruslan.php.dev@gmail.com>
2020-05-06 10:09:52 +0300

    added groups and members

---
+++ manager/src/Model/Work/UseCase/Members/Group/Edit/Handler.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace App\Model\Work\UseCase\Members\Group\Edit;
+
+
+use App\Model\Flusher;
+use App\Model\Work\Entity\Members\Group\GroupRepository;
+use App\Model\Work\Entity\Members\Group\Id;
+
+class Handler
+{
+    /**
+     * @var GroupRepository
+     */
+    private $groups;
+    /**
+     * @var Flusher
+     */
+    private $flusher;
+
+    public function __construct(GroupRepository $groups, Flusher $flusher)
+    {
+        $this->groups = $groups;
+        $this->flusher = $flusher;
+    }
+
+    public function handle(Command $command): void
+    {
+        $group = $this->groups->get(new Id($command->id));
+
+        $group->edit($command->name);
+
+        $this->flusher->flush();
+    }
+}
