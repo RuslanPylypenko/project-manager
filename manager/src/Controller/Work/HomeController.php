913829b6c339e4391c6d08b128e0700c58faa3a6
ruslan <ruslan.php.dev@gmail.com>
2020-05-10 12:08:36 +0300

    add project controllers

--- manager/src/Controller/Work/HomeController.php
+++ manager/src/Controller/Work/HomeController.php
@@ -14,6 +14,6 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->redirectToRoute('work.members');
+        return $this->redirectToRoute('work.projects');
     }
 }
---
+++ manager/src/Controller/Work/Projects/Project/ProjectController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller\Work\Projects\Project;
+
+use App\Model\Work\Entity\Projects\Project\Project;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/work/projects/{id}", name="work.projects.project")
+ */
+class ProjectController extends AbstractController
+{
+    /**
+     * @Route("", name=".show", requirements={"id"=Guid::PATTERN})
+     * @param Project $project
+     * @return Response
+     */
+    public function show(Project $project): Response
+    {
+        return $this->render('app/work/projects/project/show.html.twig', compact('project'));
+    }
+}
