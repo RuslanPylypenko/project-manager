66d1af080f4bd079ba1f72242417a294004c5cc4
ruslan <ruslan.php.dev@gmail.com>
2020-05-12 19:38:27 +0300

    update symphony

--- manager/src/Model/Work/Entity/Members/Group/IdType.php
+++ manager/src/Model/Work/Entity/Members/Group/IdType.php
@@ -23,4 +23,9 @@ class IdType extends GuidType
     {
         return self::NAME;
     }
+
+    public function requiresSQLCommentHint(AbstractPlatform $platform) : bool
+    {
+        return true;
+    }
 }
