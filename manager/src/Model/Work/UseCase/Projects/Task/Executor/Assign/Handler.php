9081337b1031027ba50cec7b12cc34ac142a43fb
ruslan <ruslan.php.dev@gmail.com>
2020-06-13 12:38:37 +0300

    Added task executors usecases

---
+++ manager/src/Model/Work/UseCase/Projects/Task/Executor/Assign/Handler.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App\Model\Work\UseCase\Projects\Task\Executor\Assign;
+
+
+use App\Model\Flusher;
+use App\Model\Work\Entity\Members\Member\Id as MemberId;
+use App\Model\Work\Entity\Members\Member\MemberRepository;
+use App\Model\Work\Entity\Projects\Task\Id;
+use App\Model\Work\Entity\Projects\Task\TaskRepository;
+
+class Handler
+{
+    private $tasks;
+    private $flusher;
+    private $members;
+
+    public function __construct(
+        TaskRepository $tasks,
+        MemberRepository $members,
+        Flusher $flusher
+    )
+    {
+        $this->tasks = $tasks;
+        $this->flusher = $flusher;
+        $this->members = $members;
+    }
+
+    public function handle(Command $command): void
+    {
+        $task = $this->tasks->get(new Id($command->id));
+
+        foreach ($command->members as $id) {
+            $member = $this->members->get(new MemberId($id));
+            if (!$task->hasExecutor($member->getId())) {
+                $task->assignExecutor($member);
+            }
+        }
+
+        $this->flusher->flush();
+    }
+}
