a735e3b208cac9b475821610794f158e0ab35b9c
ruslan <ruslan.php.dev@gmail.com>
2020-06-13 10:43:51 +0300

    Added task executors

--- manager/src/Model/Work/Entity/Projects/Task/Task.php
+++ manager/src/Model/Work/Entity/Projects/Task/Task.php
@@ -4,8 +4,10 @@
 namespace App\Model\Work\Entity\Projects\Task;
 
 
+use App\Model\Work\Entity\Members\Member\Id as MemberId;
 use App\Model\Work\Entity\Members\Member\Member;
 use App\Model\Work\Entity\Projects\Project\Project;
+use Doctrine\Common\Collections\ArrayCollection;
 use Webmozart\Assert\Assert;
 
 class Task
@@ -53,6 +55,8 @@ class Task
 
     private $status;
 
+    private $executors;
+
     public function __construct(
         Id $id,
         Project $project,
@@ -74,6 +78,7 @@ class Task
         $this->content = $content;
         $this->priority = $priority;
         $this->status = Status::new();
+        $this->executors = new ArrayCollection();
 
     }
 
@@ -145,6 +150,35 @@ class Task
         $this->priority = $priority;
     }
 
+    public function hasExecutor(MemberId $id): bool
+    {
+        foreach ($this->executors as $executor) {
+            if ($executor->getId()->isEqual($id)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function assignExecutor(Member $executor): void
+    {
+        if ($this->executors->contains($executor)) {
+            throw new \DomainException('Executor is already assigned.');
+        }
+        $this->executors->add($executor);
+    }
+
+    public function revokeExecutor(MemberId $id): void
+    {
+        foreach ($this->executors as $current) {
+            if ($current->getId()->isEqual($id)) {
+                $this->executors->removeElement($current);
+                return;
+            }
+        }
+        throw new \DomainException('Executor is not assigned.');
+    }
+
     public function isNew(): bool
     {
         return $this->status->isNew();
@@ -208,5 +242,13 @@ class Task
     public function getStatus(): Status
     {
         return $this->status;
+    }
+
+    /**
+     * @return Member[]
+     */
+    public function getExecutors(): array
+    {
+        return $this->executors->toArray();
     }
 }
