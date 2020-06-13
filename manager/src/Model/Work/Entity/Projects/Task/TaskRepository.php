979a19bed194ac18a14d6ed7eb4fcba6c72aebaa
ruslan <ruslan.php.dev@gmail.com>
2020-06-13 11:33:38 +0300

    Added task edit usecase

--- manager/src/Model/Work/Entity/Projects/Task/TaskRepository.php
+++ manager/src/Model/Work/Entity/Projects/Task/TaskRepository.php
@@ -4,6 +4,7 @@
 namespace App\Model\Work\Entity\Projects\Task;
 
 
+use App\Model\EntityNotFoundException;
 use Doctrine\ORM\EntityManagerInterface;
 
 class TaskRepository
@@ -13,12 +14,22 @@ class TaskRepository
      */
     private $em;
     private $connection;
+    private $repo;
 
     public function __construct(EntityManagerInterface $em)
     {
-
+        $this->repo = $em->getRepository(Task::class);
         $this->em = $em;
         $this->connection = $em->getConnection();
+    }
+
+    public function get(Id $id): Task
+    {
+        /** @var Task $task */
+        if (!$task = $this->repo->find($id->getValue())) {
+            throw new EntityNotFoundException('Task is not found.');
+        }
+        return $task;
     }
 
     public function add(Task $task): void
